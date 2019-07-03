8978822bdd672e9388931bd0e9ab648ba3431b3d
Joshy Orndorff <admin@joshyorndorff.com>
2019-07-03 11:57:29 -0400

    Sketch out common trait

---
+++ runtime/src/reputation_trait.rs
@@ -0,0 +1,25 @@
+pub trait Reputation<AccountId> {
+    /// The reputational score of an account. (Probably u32 or some token)
+    type Score;
+
+    /// The kind of feedback that will be given what ratings are assigned
+    type Feedback;
+
+    /// One account assigns a rating to another.
+    /// In general this may affect both of their reputations.
+    fn rate(rater: &AccountId, ratee: &AccountId, feedback: Self::Feedback)
+      -> Result<(), &'static str>;
+
+    // Create a feedback form. A feedback form must exist in order for a user to rate another user.
+    // This is an idea that may be wise or unnecessary, but for now we omit it.
+    //fn issue_form(rater: &AccountIt, ratee: &AccountId);
+
+    /// The current reputation of an account
+    fn reputation(who : &AccountId) -> Self::Score;
+}
+
+pub enum SimpleFeedback {
+    Positive,
+    Neutral,
+    Negative,
+}
