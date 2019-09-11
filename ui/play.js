f9b454dc7ea37ae0546be2e96fc0765c7a74d42e
Joshy Orndorff <admin@joshyorndorff.com>
2019-09-11 10:56:29 -0800

    Trying to initialize a keyring in play file

--- ui/play.js
+++ ui/play.js
@@ -1,4 +1,6 @@
-const { ApiPromise } = require('@polkadot/api');
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { Keyring } = require('@polkadot/keyring');
+const { cryptoWaitReady } = require('@polkadot/util-crypto');
 
 const TYPES = {
   "ListingId": "u32",
@@ -26,20 +28,35 @@ const TYPES = {
 };
 
 async function main() {
-  // initialise via static create
+  // Initialise API via static create
   const api = await ApiPromise.create({
+    provider: new WsProvider('ws://localhost:9944'),
     types: TYPES,
   });
 
-  // TODO setup initial conditions.
+  // Thought this shouldn't be necessary since I'm creating the
+  // keyring after awating for the api...
+  // https://polkadot.js.org/api/start/keyring.html#creating-a-keyring-instance
+  await cryptoWaitReady();
+
+  // Create the keyring and add the well-known Alice account
+  const keyring = new Keyring({ type: 'sr25519' });
+  const alice = keyring.addFromUri('//Alice', { name: 'Alice'}); //Error here
+  console.log(alice);
+
+  // Setup initial conditions.
   // Alice posts an item for sale.
-  // let price = ??;
-  // let description = ??;
-  //api.tx.marketplace.postListing(price, description)
+  let price = 100;
+  let description = 1234567890;
+  //api.tx.marketplace.postListing(price, description).signAndSend(alice);
 
   // Tell me about listing 0
   let listing0 = await api.query.marketplace.statuses(0);
-  console.log(JSON.stringify(listing0));
+  console.log(`Listing 0 isSome: ${listing0.isSome}`);
+  console.log(`Listing 0 isNone: ${listing0.isNone}`);
+
+
+  // listing0 = listing0.unwrap();
   // console.log(`Listing is active: ${listing0.isActive}`);
   // console.log(`Listing is sold: ${listing0.isSold}`);
   // console.log(`Listing is buyer reviewed: ${listing0.isBuyerReviewed}`);
