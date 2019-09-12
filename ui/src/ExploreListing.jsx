9f3a1b4d665a7ef79ae9c4fc2ec4193b76ce7c47
Joshy Orndorff <admin@joshyorndorff.com>
2019-09-11 16:07:59 -0800

    Pretty stuck on displaying listings correctly.

--- ui/src/ExploreListing.jsx
+++ ui/src/ExploreListing.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Grid } from "semantic-ui-react";
+import { Form, Input, Grid, Table } from "semantic-ui-react";
 
 import TxButton from "./TxButton";
 
 export default function ExploreListing(props) {
   const { api, accountPair } = props;
   const [listingId, setListingId] = useState(0);
-  const [listing, setListing] = useState({ isEmpty: () => true });
-  const [buyer, setBuyer] = useState({ isEmpty: () => true });
-  const [status, setStatus] = useState({ isEmpty: () => true });
+  const [listing, setListing] = useState("initial");
+  const [buyer, setBuyer] = useState("initial");
+  const [status, setStatus] = useState("initial");
+  // Tried using this dummy object as initial state
+  // { isEmpty: () => true }
 
   // Load the listing in question
   // TODO Consider allowing user to view multiple listings at once
@@ -19,15 +21,21 @@ export default function ExploreListing(props) {
       [api.query.marketplace.listings, listingId],
       [api.query.marketplace.buyers,   listingId],
       [api.query.marketplace.statuses, listingId],
-    ], ([listing, buyer, status]) => {
-      console.log(`got some results ${listing}, ${buyer}, ${status}`);
+    ], ([l, b, s]) => {
+      console.log(`got some results ${l}, ${b}, ${s}`);
+      setListing(l);
+      setBuyer(b);
+      setStatus(s);
+      // State variables seem to update Correctly. Why doesn't the table below update?
+      console.log(`state variables are ${listing}, ${buyer}, ${status}`);
     })
     .then(u => {
       unsubscribe = u;
     })
     .catch(console.error);
 
-  }, [listingId, accountPair]);
+    return () => unsubscribe && unsubscribe();
+  }, /*[listingId, accountPair]*/);
 
 
 
@@ -37,6 +45,7 @@ export default function ExploreListing(props) {
       <Form>
         <Form.Field>
           <Input
+            value={0}
             type="number"
             id="listing_is"
             label="Listing ID"
@@ -44,6 +53,33 @@ export default function ExploreListing(props) {
           />
         </Form.Field>
       </Form>
+      <Table celled striped size="small">
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell textAlign="right">Seller</Table.Cell>
+            <Table.Cell textAlign="left">{listing.seller}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign="right">Price</Table.Cell>
+            <Table.Cell textAlign="left">{listing.price}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign="right">Description</Table.Cell>
+            <Table.Cell textAlign="left">{listing.description}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <Grid.Row>
+      <TxButton
+        api={api}
+        accountPair={accountPair}
+        label={"Buy"}
+        params={[listingId]}
+        setStatus={setStatus}
+        disabled={status !== "Active"}
+        tx={api.tx.marketplace.buy}
+      />
+      </Grid.Row>
     </Grid.Column>
   );
 }
